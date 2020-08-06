92df46343f3cdc9dcb9c11875c5976e67452e700
Riven <lrw90316@163.com>
2020-08-06 23:19:00 +0800

    测试 __await__的使用

--- app_server/tests/test.py
+++ app_server/tests/test.py
@@ -8,5 +8,60 @@
 @版本        :1.0.0
 '''
 
+from typing import (
+    Any,
+    AsyncIterator,
+    Callable,
+    Generator,
+    Iterable,
+    Optional,
+    Type,
+    Union,
+)
+
+import asyncio
+import threading
+
+class B:
+    def __iter__(self):
+        return self
+    def __next__(self):
+        raise StopIteration('end')
+
+
+class A:
+    def __await__(self):
+        print('__await__-------start')
+        t = self.test()
+        print(t)
+        print('__await__-------end')
+        return t.__await__()
+        # asyncio.sleep(1)
+        # return self.__await__()
+
+    async def test(self):
+        await asyncio.sleep(1)
+        return self
+
+    async def good(self):
+        await asyncio.sleep(2)
+        print('22222')
+        return 500
+
+async def a():
+    s = await A()
+    # s = A()
+    print(s)
+
+    s1 = await s
+    print(s1)
+
+    # t1 = await s.good()
+    # print(t1)
+
+
+
 if __name__ == '__main__':
-    print(__file__)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(a())
+    loop.close()
