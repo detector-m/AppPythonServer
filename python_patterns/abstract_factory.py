219cd591c7d2c29e7f65fb897f11e92ea2649acf
Riven <lrw90316@163.com>
2020-08-10 22:51:37 +0800

    新增设计模式测试

---
+++ python_patterns/abstract_factory.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件        :abstract_factory.py
+@说明        : 抽象工厂模式-> http://www.pythontip.com/python-patterns/detail/abstract_factory
+https://www.cnblogs.com/onepiece-andy/p/python-abstract-factory-pattern.html
+@时间        :2020/08/10 22:40:22
+@作者        :Riven
+@版本        :1.0.0
+'''
+
+
+
