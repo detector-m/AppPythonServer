647832e1cbc3b3fc7328269ea0b4e8ebcc8170a3
Riven <ruiwen.long@ioa365.com>
2020-08-18 14:54:29 +0800

    完善策略模式

--- python_patterns/strategy.py
+++ python_patterns/strategy.py
@@ -39,33 +39,132 @@ https://developer.aliyun.com/article/71071
 '''
 
 # 方式一
-import types
+# import types
 
-class StrategyExample:
-    def __init__(self, func=None):
-        self.name = 'Strategy Example 0'
-        if func is not None:
-            self.execute = types.MethodType(func, self)
+# class StrategyExample:
+#     def __init__(self, func=None):
+#         self.name = 'Strategy Example 0'
+#         if func is not None:
+#             self.execute = types.MethodType(func, self)
 
-    def execute(self):
-        print(self.name)
+#     def execute(self):
+#         print(self.name)
 
-def execute_replacement1(self):
-    print(self.name + 'from execute 1')
+# def execute_replacement1(self):
+#     print(self.name + 'from execute 1')
 
-def execute_replacement2(self):
-    print(self.name + 'from execute 2')
+# def execute_replacement2(self):
+#     print(self.name + 'from execute 2')
+
+# if __name__ == '__main__':
+#     strat0 = StrategyExample()
+ 
+#     strat1 = StrategyExample(execute_replacement1)
+#     strat1.name = 'Stragety 1'
+
+#     strat2 = StrategyExample(execute_replacement2)
+#     strat2.name = 'Strategy 2'
+
+#     strat0.execute()
+#     strat1.execute()
+#     strat2.execute()
+
+# 方式二
+from abc import ABCMeta, abstractclassmethod
+import random
+
+class Sort(metaclass=ABCMeta):
+    @abstractclassmethod
+    def sort(self, data):
+        pass
+
+class QuickSort(Sort):
+    def quick_sort(self, data, left, right):
+        if left < right:
+            mid = self.partition(data, left, right)
+            self.quick_sort(data, left, mid-1)
+            self.quick_sort(data, mid+1, right)
+
+    def partition(self, data, left, right):
+        tmp = data[left]
+        while left < right:
+            while left < right and data[right] >= tmp:
+                right -= 1
+            data[left] = data[right]
+
+            while left < right and data[left] <= tmp:
+                left += 1
+            data[right] = data[left]
+
+        data[left] = tmp
+
+        return left
+
+
+    def sort(self, data):
+        print('快速排序')
+        return self.quick_sort(data, 0, len(data)-1)
+
+class MergeSort(Sort):
+    def merge_sort(self, data, low, high):
+        if low < high:
+            mid = (low + high) // 2
+            self.merge_sort(data, low, mid)
+            self.merge_sort(data, mid+1, high)
+            self.merge(data, low, mid, high)
+
+    def merge(self, data, low, mid, high):
+        i = low
+        j = mid + 1
+        ltmp = []
+
+        while i<=mid and j<=high:
+            if data[i] <= data[j]:
+                ltmp.append(data[i])
+                i += 1
+            else:
+                ltmp.append(data[j])
+                j += 1
+        
+        while i<=mid:
+            ltmp.append(data[i])
+            i += 1
+
+        while j<=high:
+            ltmp.append(data[j])
+            j += 1
+
+        data[low: high+1] = ltmp
+
+    def sort(self, data):
+        print('归并排序')
+        return self.merge_sort(data, 0, len(data)-1)
+
+class Context:
+    def __init__(self, data, strategy=None):
+        self.data = data
+        self.strategy = strategy
+    
+    def set_strategy(self, strategy):
+        self.strategy = strategy
+
+    def do_strategy(self):
+        if self.strategy:
+            self.strategy.sort(self.data)
+        else:
+            raise TypeError
+
+    
 
 if __name__ == '__main__':
-    strat0 = StrategyExample()
- 
-    strat1 = StrategyExample(execute_replacement1)
-    strat1.name = 'Stragety 1'
+    li = list(range(10000))
+    random.shuffle(li)
 
-    strat2 = StrategyExample(execute_replacement2)
-    strat2.name = 'Strategy 2'
+    context = Context(li, MergeSort())
+    context.do_strategy()
 
-    strat0.execute()
-    strat1.execute()
-    strat2.execute()
+    random.shuffle(context.data)
+
+    context.set_strategy(QuickSort())
+    context.do_strategy()
 
