0bf4b7461544411d3a5d95761406d622a8279e7d
Riven <ruiwen.long@ioa365.com>
2020-08-27 16:16:45 +0800

    原型模式的第二种实现

--- python_patterns/prototype.py
+++ python_patterns/prototype.py
@@ -21,6 +21,7 @@ https://www.cnblogs.com/onepiece-andy/p/python_prototype_pattern.html
 当一个类的实例只能有几个不同状态组合中的一种时。建立相应数目的原型并克隆它们可能比每次用合适的状态手工实例化该类更方便一些。
 '''
 
+# 方式1
 import copy
 
 class Prototype:
@@ -59,3 +60,67 @@ def main():
     
 if __name__ == '__main__':
     main()
+
+# 方式2
+# from abc import abstractclassmethod, ABCMeta
+# import copy
+
+# # 原型抽象类
+# class Prototype(metaclass=ABCMeta):
+#     @abstractclassmethod
+#     def clone(self):
+#         pass
+
+#     @abstractclassmethod
+#     def deep_clone(self):
+#         pass
+
+# # 工作经历
+# class WorkExperience:
+#     def __init__(self):
+#         self.timearea = ''
+#         self.company = ''
+
+#     def set_workexperience(self, timearea, company):
+#         self.timearea = timearea
+#         self.company = company
+
+# # 简历类
+# class Resume(Prototype):
+#     def __init__(self, name):
+#         self.name = name
+#         self.workexperience = WorkExperience()
+
+#     def set_personinfo(self, sex, age):
+#         self.sex = sex
+#         self.age = age
+
+#     def set_workexperience(self, timearea, company):
+#         self.workexperience.set_workexperience(timearea, company)
+
+#     def display(self):
+#         print(self.name)
+#         print(f'{self.sex}, {self.age}')
+#         print(f'工作经历 {self.workexperience.timearea}, {self.workexperience.company}')
+
+#     def clone(self):
+#         return copy.copy(self)
+
+#     def deep_clone(self):
+#         return copy.deepcopy(self)
+
+# if __name__ == "__main__":
+#     obj1 = Resume('andy')
+#     obj2 = obj1.clone()  # 浅拷贝对象
+#     obj3 = obj1.deep_clone()  # 深拷贝对象
+
+#     obj1.set_personinfo('男', 28)
+#     obj1.set_workexperience('2010-2015', 'AA')
+#     obj2.set_personinfo('女', 27)
+#     obj2.set_workexperience('2011-2019', 'BB')  # 修改浅拷贝的对象工作经历
+#     obj3.set_personinfo('男', 29)
+#     obj3.set_workexperience('2016-2020', 'CC')  # 修改深拷贝的对象的工作经历
+    
+#     obj1.display()
+#     obj2.display()
+#     obj3.display()
